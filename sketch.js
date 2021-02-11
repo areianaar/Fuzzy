4edd5a0a75c45af2f43a54c87f7f2cdea18c32b7
Areiana Richards <areianarichards@Areianas-MacBook-Air.local>
2021-02-10 22:09:35 -0600

    changed alert and stroke color

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 function setup() {
     createCanvas(displayWidth, displayHeight);
-    alert("Key statement for peice titled Itchy. Use mouse to manuever icon around the screen to fill background and add texture to the artwork. I wanted to elicit a sense of uneasiness within the user and i felt the best way to do so was to choose colors that contrasted with one another but were also complimentary so it doesnt completley burn the retinas. The amount of arms the icon has and the decision to have its shadow build up over the background adds a unique texture to the peice each time its opened so it is unsettling and in turn also adds depth to it as well")
+    alert("Key statement for peice titled Itchy. Use mouse to manuever icon around the screen to fill background and add texture to the artwork. I wanted to elicit a sense of uneasiness within the user and I felt the best way to do so was to choose colors that contrasted with one another but were also complimentary. The amount of arms the icon has and the decision to have its shadow build up over the background adds a unique texture to the peice each time its opened so it is unsettling and in turn also adds depth to it as well")
     background(225, 216, 18)
   }
   
   function draw() {
   translate(width * 0.0, height * 0.0);
   star(mouseX, mouseY, 30, 70, 25);
-  stroke(150, 50, 168);
+  stroke(170, 50, 168);
   fill(66, 50, 168);
 
   function star(x, y, radius1, radius2, npoints) {
